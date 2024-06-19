69e02a9f06f7fa0de977441a1cf1a5257dbe183a
sfyr111 <sfyr111@163.com>
2024-06-19 14:18:56 +0800

    feat(crypto): Implement test coverage and address & sign

--- crypto/keys.go
+++ crypto/keys.go
@@ -3,6 +3,8 @@ package crypto
 import (
 	"crypto/ed25519"
 	"crypto/rand"
+	"encoding/hex"
+	"fmt"
 	"io"
 )
 
@@ -10,10 +12,26 @@ const (
 	privKeyLen = 64
 	pubKeyLen  = 32
 	seedLen    = 32
+	addressLen = 20
 )
 
 type PrivateKey struct {
 	key ed25519.PrivateKey
+}
+
+func NewPrivateKeyFromString(s string) *PrivateKey {
+	b, err := hex.DecodeString(s)
+	if err != nil {
+		panic(err)
+	}
+	return NewPrivateKeyFromSeed(b)
+}
+
+func NewPrivateKeyFromSeed(seed []byte) *PrivateKey {
+	if len(seed) != seedLen {
+		panic("seed length must be 32")
+	}
+	return &PrivateKey{key: ed25519.NewKeyFromSeed(seed)}
 }
 
 func GeneratePrivateKey() *PrivateKey {
@@ -31,20 +49,59 @@ func (p *PrivateKey) Bytes() []byte {
 	return p.key
 }
 
-func (p *PrivateKey) Sign(message []byte) []byte {
-	return ed25519.Sign(p.key, message)
+func (p *PrivateKey) Sign(message []byte) *Signature {
+	return &Signature{
+		value: ed25519.Sign(p.key, message),
+	}
 }
 
 func (p *PrivateKey) Public() *PublicKey {
-	return &PublicKey{key: p.key.Public().(ed25519.PublicKey)}
+	//return &PublicKey{key: p.key.Public().(ed25519.PublicKey)}
 
-	//b := make([]byte, pubKeyLen)
-	//copy(b, p.key[32:])
-	//return &PublicKey{
-	//	key: b,
-	//}
+	b := make([]byte, pubKeyLen)
+	fmt.Printf("Before copy: %x\n", b)
+	copy(b, p.key[32:])
+	fmt.Printf("After copy: %x\n", b)
+
+	return &PublicKey{
+		key: b,
+	}
+}
+
+func (p *PublicKey) Address() Address {
+	return Address{
+		value: p.key[len(p.key)-addressLen:],
+	}
 }
 
 type PublicKey struct {
 	key ed25519.PublicKey
+}
+
+func (p *PublicKey) Bytes() []byte {
+	return p.key
+}
+
+type Signature struct {
+	value []byte
+}
+
+func (s *Signature) Bytes() []byte {
+	return s.value
+}
+
+func (s *Signature) Verify(pubKey *PublicKey, message []byte, sig *Signature) bool {
+	return ed25519.Verify(pubKey.key, message, sig.value)
+}
+
+type Address struct {
+	value []byte
+}
+
+func (a Address) Bytes() []byte {
+	return a.value
+}
+
+func (a Address) String() string {
+	return hex.EncodeToString(a.value)
 }
--- crypto/keys_test.go
+++ crypto/keys_test.go
@@ -9,4 +9,40 @@ func TestGeneratePrivateKey(t *testing.T) {
 	privKey := GeneratePrivateKey()
 
 	assert.Equal(t, privKeyLen, len(privKey.Bytes()))
+
+	pubKey := privKey.Public()
+	assert.Equal(t, pubKeyLen, len(pubKey.Bytes()))
+}
+
+func TestPrivateKeySign(t *testing.T) {
+	privKey := GeneratePrivateKey()
+	pubKey := privKey.Public()
+	msg := []byte("foo bar baz")
+
+	sig := privKey.Sign(msg)
+	assert.True(t, sig.Verify(pubKey, msg, sig))
+
+	// Test with invalid msg
+	assert.False(t, sig.Verify(pubKey, []byte("foo"), sig))
+
+	// Test with invalid pubKey
+	invalidPrivKey := GeneratePrivateKey()
+	invalidPubKey := invalidPrivKey.Public()
+	assert.False(t, sig.Verify(invalidPubKey, msg, sig))
+}
+
+func TestNewPrivateKeyFromString(t *testing.T) {
+	//seed := make([]byte, 32)
+	//io.ReadFull(rand.Reader, seed)
+	//fmt.Println(hex.EncodeToString(seed))
+
+	var (
+		seed       = "4a403c3608ef520f4a677e56f3147408e0183b43ad36a0168e6c79945a603c11"
+		privKey    = NewPrivateKeyFromString(seed)
+		addressStr = "33b326adda5e6cad6918923de2259f787e1a7a1b"
+	)
+
+	assert.Equal(t, privKeyLen, len(privKey.Bytes()))
+	address := privKey.Public().Address()
+	assert.Equal(t, addressStr, address.String())
 }
